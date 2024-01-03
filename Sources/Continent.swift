542bc1567396b61960b23fd6c62b644c86798929
gosha <gosha.titov@icloud.com>
2024-01-03 16:08:34 +0300

    Feat: add the `WPContinent` enum

---
+++ Sources/Continent.swift
@@ -0,0 +1,12 @@
+/// A continent that is any of several large geographical regions.
+///
+/// The list of continents: `Asia`, `Africa`, `Europe`, `NorthAmerica`, `SouthAmerica`, `Australia` and `Antartica`.
+public enum WPContinent: CaseIterable {
+    case Asia
+    case Africa
+    case Europe
+    case NorthAmerica
+    case SouthAmerica
+    case Australia
+    case Antartica
+}
