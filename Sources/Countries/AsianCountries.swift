cee2402cd3dcc0b41ca5f0e910d450a7873cd710
gosha <gosha.titov@icloud.com>
2024-01-03 16:19:40 +0300

    Feat: add Asian countries

---
+++ Sources/Countries/AsianCountries.swift
@@ -0,0 +1,225 @@
+extension WPCountry {
+    
+    /// Returns all countries located on the Asia continent.
+    public static var allAsianCountries: [WPCountry] {
+        return [
+            Afghanistan, Armenia, Azerbaijan,
+            Bahrain, Bangladesh, Bhutan, Brunei,
+            Cambodia, China,
+            Georgia,
+            India, Indonesia, Iran, Iraq, Israel,
+            HongKong,
+            Japan, Jordan,
+            Kazakhstan, Kuwait, Kyrgyzstan,
+            Laos, Lebanon,
+            Malaysia, Maldives, Mongolia, Myanmar,
+            Nepal, NorthKorea,
+            Oman,
+            Pakistan, Palestine, Philippines,
+            Qatar,
+            SaudiArabia, Singapore, SouthKorea, SriLanka, Syria,
+            Taiwan, Tajikistan, Thailand, TimorLeste, Turkey, Turkmenistan,
+            UnitedArabEmirates, Uzbekistan,
+            Vietnam,
+            Yemen,
+        ]
+    }
+    
+    
+    public static var Afghanistan: WPCountry {
+        return WPCountry(systemName: "Afghanistan", isoCode: "AF", continent: .Asia, population: 38_928_346, area: 652_230)
+    }
+    
+    public static var Armenia: WPCountry {
+        return WPCountry(systemName: "Armenia", isoCode: "AM", continent: .Asia, population: 2_777_974, area: 29_743)
+    }
+    
+    public static var Azerbaijan: WPCountry {
+        return WPCountry(systemName: "Azerbaijan", isoCode: "AZ", continent: .Asia, population: 10_436_000, area: 86_600)
+    }
+    
+    public static var Bahrain: WPCountry {
+        return WPCountry(systemName: "Bahrain", isoCode: "BH", continent: .Asia, population: 1_701_575, area: 760)
+    }
+    
+    public static var Bangladesh: WPCountry {
+        return WPCountry(systemName: "Bangladesh", isoCode: "BD", continent: .Asia, population: 164_689_383, area: 147_570)
+    }
+    
+    public static var Bhutan: WPCountry {
+        return WPCountry(systemName: "Bhutan", isoCode: "BT", continent: .Asia, population: 771_608, area: 38_394)
+    }
+    
+    public static var Brunei: WPCountry {
+        return WPCountry(systemName: "Brunei", isoCode: "BN", continent: .Asia, population: 437_479, area: 5_765)
+    }
+    
+    public static var Cambodia: WPCountry {
+        return WPCountry(systemName: "Cambodia", isoCode: "KH", continent: .Asia, population: 16_718_965, area: 181_035)
+    }
+    
+    public static var China: WPCountry {
+        return WPCountry(systemName: "China", isoCode: "CN", continent: .Asia, population: 1_439_323_776, area: 9_596_961)
+    }
+    
+    public static var Georgia: WPCountry {
+        return WPCountry(systemName: "Georgia", isoCode: "GE", continent: .Asia, population: 3_989_167, area: 69_700)
+    }
+    
+    public static var India: WPCountry {
+        return WPCountry(systemName: "India", isoCode: "IN", continent: .Asia, population: 1_380_004_385, area: 3_287_263)
+    }
+    
+    public static var Indonesia: WPCountry {
+        return WPCountry(systemName: "Indonesia", isoCode: "ID", continent: .Asia, population: 273_523_615, area: 1_904_569)
+    }
+    
+    public static var Iran: WPCountry {
+        return WPCountry(systemName: "Iran", isoCode: "IR", continent: .Asia, population: 83_992_949, area: 1_648_195)
+    }
+    
+    public static var Iraq: WPCountry {
+        return WPCountry(systemName: "Iraq", isoCode: "IQ", continent: .Asia, population: 40_222_493, area: 438_317)
+    }
+    
+    public static var Israel: WPCountry {
+        return WPCountry(systemName: "Israel", isoCode: "IL", continent: .Asia, population: 8_655_535, area: 22_072)
+    }
+    
+    public static var HongKong: WPCountry {
+        return WPCountry(systemName: "HongKong", isoCode: "HK", continent: .Asia, population: 7_494_174, area: 2_755)
+    }
+    
+    public static var Japan: WPCountry {
+        return WPCountry(systemName: "Japan", isoCode: "JP", continent: .Asia, population: 126_476_461, area: 377_975)
+    }
+    
+    public static var Jordan: WPCountry {
+        return WPCountry(systemName: "Jordan", isoCode: "JO", continent: .Asia, population: 10_203_134, area: 89_342)
+    }
+    
+    public static var Kazakhstan: WPCountry {
+        return WPCountry(systemName: "Kazakhstan", isoCode: "KZ", continent: .Asia, population: 18_776_707, area: 2_724_900)
+    }
+    
+    public static var Kuwait: WPCountry {
+        return WPCountry(systemName: "Kuwait", isoCode: "KW", continent: .Asia, population: 4_270_571, area: 17_818)
+    }
+    
+    public static var Kyrgyzstan: WPCountry {
+        return WPCountry(systemName: "Kyrgyzstan", isoCode: "KG", continent: .Asia, population: 6_524_195, area: 199_951)
+    }
+    
+    public static var Laos: WPCountry {
+        return WPCountry(systemName: "Laos", isoCode: "LA", continent: .Asia, population: 7_275_560, area: 236_800)
+    }
+    
+    public static var Lebanon: WPCountry {
+        return WPCountry(systemName: "Lebanon", isoCode: "LB", continent: .Asia, population: 6_825_445, area: 10_400)
+    }
+    
+    public static var Malaysia: WPCountry {
+        return WPCountry(systemName: "Malaysia", isoCode: "MY", continent: .Asia, population: 32_365_999, area: 330_803)
+    }
+    
+    public static var Maldives: WPCountry {
+        return WPCountry(systemName: "Maldives", isoCode: "MV", continent: .Asia, population: 540_544, area: 298)
+    }
+    
+    public static var Mongolia: WPCountry {
+        return WPCountry(systemName: "Mongolia", isoCode: "MN", continent: .Asia, population: 3_278_290, area: 1_566_500)
+    }
+    
+    public static var Myanmar: WPCountry {
+        return WPCountry(systemName: "Myanmar", isoCode: "MM", continent: .Asia, population: 54_409_800, area: 676_578)
+    }
+    
+    public static var Nepal: WPCountry {
+        return WPCountry(systemName: "Nepal", isoCode: "NP", continent: .Asia, population: 29_136_808, area: 147_181)
+    }
+    
+    public static var NorthKorea: WPCountry {
+        return WPCountry(systemName: "NorthKorea", isoCode: "KP", continent: .Asia, population: 25_778_816, area: 120_538)
+    }
+    
+    public static var Oman: WPCountry {
+        return WPCountry(systemName: "Oman", isoCode: "OM", continent: .Asia, population: 5_106_626, area: 309_500)
+    }
+    
+    public static var Pakistan: WPCountry {
+        return WPCountry(systemName: "Pakistan", isoCode: "PK", continent: .Asia, population: 220_892_340, area: 881_913)
+    }
+    
+    public static var Palestine: WPCountry {
+        return WPCountry(systemName: "Palestine", isoCode: "PS", continent: .Asia, population: 5_432_505, area: 6_020)
+    }
+    
+    public static var Philippines: WPCountry {
+        return WPCountry(systemName: "Philippines", isoCode: "PH", continent: .Asia, population: 109_581_078, area: 300_000)
+    }
+    
+    public static var Qatar: WPCountry {
+        return WPCountry(systemName: "Qatar", isoCode: "QA", continent: .Asia, population: 2_881_053, area: 11_586)
+    }
+    
+    public static var SaudiArabia: WPCountry {
+        return WPCountry(systemName: "SaudiArabia", isoCode: "SA", continent: .Asia, population: 34_813_871, area: 2_149_690)
+    }
+    
+    public static var Singapore: WPCountry {
+        return WPCountry(systemName: "Singapore", isoCode: "SG", continent: .Asia, population: 5_850_342, area: 710)
+    }
+    
+    public static var SouthKorea: WPCountry {
+        return WPCountry(systemName: "SouthKorea", isoCode: "KR", continent: .Asia, population: 51_269_185, area: 100_210)
+    }
+    
+    public static var SriLanka: WPCountry {
+        return WPCountry(systemName: "SriLanka", isoCode: "LK", continent: .Asia, population: 21_413_249, area: 65_610)
+    }
+    
+    public static var Syria: WPCountry {
+        return WPCountry(systemName: "Syria", isoCode: "SY", continent: .Asia, population: 17_500_658, area: 185_180)
+    }
+    
+    public static var Taiwan: WPCountry {
+        return WPCountry(systemName: "Taiwan", isoCode: "TW", continent: .Asia, population: 23_816_775, area: 36_190)
+    }
+    
+    public static var Tajikistan: WPCountry {
+        return WPCountry(systemName: "Tajikistan", isoCode: "TJ", continent: .Asia, population: 9_537_645, area: 143_100)
+    }
+    
+    public static var Thailand: WPCountry {
+        return WPCountry(systemName: "Thailand", isoCode: "TH", continent: .Asia, population: 69_799_978, area: 513_120)
+    }
+    
+    public static var TimorLeste: WPCountry {
+        return WPCountry(systemName: "TimorLeste", isoCode: "TL", continent: .Asia, population: 1_369_569, area: 15_006)
+    }
+    
+    public static var Turkey: WPCountry {
+        return WPCountry(systemName: "Turkey", isoCode: "TR", continent: .Asia, population: 86_023_777, area: 783_562)
+    }
+    
+    public static var Turkmenistan: WPCountry {
+        return WPCountry(systemName: "Turkmenistan", isoCode: "TM", continent: .Asia, population: 6_031_200, area: 488_100)
+    }
+    
+    public static var UnitedArabEmirates: WPCountry {
+        return WPCountry(systemName: "UnitedArabEmirates", isoCode: "AE", continent: .Asia, population: 9_890_402, area: 83_600)
+    }
+    
+    public static var Uzbekistan: WPCountry {
+        return WPCountry(systemName: "Uzbekistan", isoCode: "UZ", continent: .Asia, population: 33_469_203, area: 447_400)
+    }
+    
+    public static var Vietnam: WPCountry {
+        return WPCountry(systemName: "Vietnam", isoCode: "VN", continent: .Asia, population: 97_338_579, area: 331_212)
+    }
+    
+    public static var Yemen: WPCountry {
+        return WPCountry(systemName: "Yemen", isoCode: "YE", continent: .Asia, population: 29_825_964, area: 527_968)
+    }
+    
+}
