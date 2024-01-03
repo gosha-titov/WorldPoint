a39a6c32fc41306d9b829ea3929c97ff61cc8abc
gosha <gosha.titov@icloud.com>
2024-01-03 16:00:41 +0300

    Chore: initial setup

--- Package.swift
+++ Package.swift
@@ -6,18 +6,20 @@ import PackageDescription
 let package = Package(
     name: "WorldPoint",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WorldPoint",
-            targets: ["WorldPoint"]),
+            targets: ["WorldPoint"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WorldPoint"),
+            name: "WorldPoint",
+            dependencies: [],
+            path: "Sources"
+        ),
         .testTarget(
             name: "WorldPointTests",
-            dependencies: ["WorldPoint"]),
+            dependencies: ["WorldPoint"]
+        ),
     ]
 )
---
+++ Sources/Country.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gosha on 03.01.2024.
+//
+
+import Foundation
