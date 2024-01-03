9c4cd645818aba4b9c2f5e84c57e14eae3802934
gosha <gosha.titov@icloud.com>
2024-01-03 17:08:54 +0300

    Feat: add European countries

---
+++ Sources/Countries/EuropeanCountries.swift
@@ -0,0 +1,236 @@
+extension WPCountry {
+    
+    /// Returns all countries located on the Europe continent.
+    public static var allEuropeanCountries: [WPCountry] {
+        return [
+            AlandIslands, Albania, Andorra, Austria,
+            Belarus, Belgium, BosniaAndHerzegovina, Bulgaria,
+            Croatia, Cyprus, Czechia,
+            Denmark,
+            Estonia,
+            FaroeIslands, Finland, France,
+            Germany, Gibraltar, Greece, Greenland, Guernsey,
+            Hungary,
+            Iceland, Ireland, IsleOfMan, Italy,
+            Kosovo,
+            Latvia, Liechtenstein, Lithuania, Luxembourg,
+            Malta, Moldova, Monaco, Montenegro,
+            Netherlands, NorthMacedonia, Norway,
+            Poland, Portugal,
+            Romania, Russia,
+            SanMarino, Serbia, Slovakia, Slovenia, Spain, Sweden, Switzerland,
+            Ukraine, UnitedKingdom,
+            VaticanCity
+        ]
+    }
+    
+    
+    public static var AlandIslands: WPCountry {
+        return WPCountry(systemName: "AlandIslands", isoCode: "AX", continent: .Europe, population: 29_789, area: 1_580)
+    }
+    
+    public static var Albania: WPCountry {
+        return WPCountry(systemName: "Albania", isoCode: "AL", continent: .Europe, population: 2_845_955, area: 28_748)
+    }
+    
+    public static var Andorra: WPCountry {
+        return WPCountry(systemName: "Andorra", isoCode: "AD", continent: .Europe, population: 77_142, area: 468)
+    }
+    
+    public static var Austria: WPCountry {
+        return WPCountry(systemName: "Austria", isoCode: "AT", continent: .Europe, population: 8_902_600, area: 83_879)
+    }
+    
+    public static var Belarus: WPCountry {
+        return WPCountry(systemName: "Belarus", isoCode: "BY", continent: .Europe, population: 9_408_400, area: 207_600)
+    }
+    
+    public static var Belgium: WPCountry {
+        return WPCountry(systemName: "Belgium", isoCode: "BE", continent: .Europe, population: 11_589_623, area: 30_528)
+    }
+    
+    public static var BosniaAndHerzegovina: WPCountry {
+        return WPCountry(systemName: "BosniaAndHerzegovina", isoCode: "BA", continent: .Europe, population: 3_301_000, area: 51_209)
+    }
+    
+    public static var Bulgaria: WPCountry {
+        return WPCountry(systemName: "Bulgaria", isoCode: "BG", continent: .Europe, population: 6_951_482, area: 110_994)
+    }
+    
+    public static var Croatia: WPCountry {
+        return WPCountry(systemName: "Croatia", isoCode: "HR", continent: .Europe, population: 4_105_267, area: 56_594)
+    }
+    
+    public static var Cyprus: WPCountry {
+        return WPCountry(systemName: "Cyprus", isoCode: "CY", continent: .Europe, population: 1_207_359, area: 9_251)
+    }
+    
+    public static var Czechia: WPCountry {
+        return WPCountry(systemName: "Czechia", isoCode: "CZ", continent: .Europe, population: 10_708_981, area: 78_866)
+    }
+    
+    public static var Denmark: WPCountry {
+        return WPCountry(systemName: "Denmark", isoCode: "DK", continent: .Europe, population: 5_806_081, area: 42_933)
+    }
+    
+    public static var Estonia: WPCountry {
+        return WPCountry(systemName: "Estonia", isoCode: "EE", continent: .Europe, population: 1_326_535, area: 45_227)
+    }
+    
+    public static var FaroeIslands: WPCountry {
+        return WPCountry(systemName: "FaroeIslands", isoCode: "FO", continent: .Europe, population: 52_889, area: 1_399)
+    }
+    
+    public static var Finland: WPCountry {
+        return WPCountry(systemName: "Finland", isoCode: "FI", continent: .Europe, population: 5_540_720, area: 338_424)
+    }
+    
+    public static var France: WPCountry {
+        return WPCountry(systemName: "France", isoCode: "FR", continent: .Europe, population: 67_081_000, area: 551_695)
+    }
+    
+    public static var Germany: WPCountry {
+        return WPCountry(systemName: "Germany", isoCode: "DE", continent: .Europe, population: 83_783_942, area: 357_022)
+    }
+    
+    public static var Gibraltar: WPCountry {
+        return WPCountry(systemName: "Gibraltar", isoCode: "GI", continent: .Europe, population: 32_669, area: 7)
+    }
+    
+    public static var Greece: WPCountry {
+        return WPCountry(systemName: "Greece", isoCode: "GR", continent: .Europe, population: 10_423_054, area: 131_957)
+    }
+    
+    public static var Greenland: WPCountry {
+        return WPCountry(systemName: "Greenland", isoCode: "GL", continent: .Europe, population: 56_653, area: 2_166_086)
+    }
+    
+    public static var Guernsey: WPCountry {
+        return WPCountry(systemName: "Guernsey", isoCode: "GG", continent: .Europe, population: 63_950, area: 62)
+    }
+    
+    public static var Hungary: WPCountry {
+        return WPCountry(systemName: "Hungary", isoCode: "HU", continent: .Europe, population: 9_660_351, area: 93_028)
+    }
+    
+    public static var Iceland: WPCountry {
+        return WPCountry(systemName: "Iceland", isoCode: "IS", continent: .Europe, population: 356_991, area: 103_000)
+    }
+    
+    public static var Ireland: WPCountry {
+        return WPCountry(systemName: "Ireland", isoCode: "IE", continent: .Europe, population: 4_937_786, area: 70_273)
+    }
+    
+    public static var IsleOfMan: WPCountry {
+        return WPCountry(systemName: "IsleOfMan", isoCode: "IM", continent: .Europe, population: 84_263, area: 572)
+    }
+    
+    public static var Italy: WPCountry {
+        return WPCountry(systemName: "Italy", isoCode: "IT", continent: .Europe, population: 60_461_826, area: 301_340)
+    }
+    
+    public static var Kosovo: WPCountry {
+        return WPCountry(systemName: "Kosovo", isoCode: "XK", continent: .Europe, population: 1_964_327, area: 10_887)
+    }
+    
+    public static var Latvia: WPCountry {
+        return WPCountry(systemName: "Latvia", isoCode: "LV", continent: .Europe, population: 1_886_198, area: 64_589)
+    }
+    
+    public static var Liechtenstein: WPCountry {
+        return WPCountry(systemName: "Liechtenstein", isoCode: "LI", continent: .Europe, population: 38_019, area: 160)
+    }
+    
+    public static var Lithuania: WPCountry {
+        return WPCountry(systemName: "Lithuania", isoCode: "LT", continent: .Europe, population: 2_722_289, area: 65_300)
+    }
+    
+    public static var Luxembourg: WPCountry {
+        return WPCountry(systemName: "Luxembourg", isoCode: "LU", continent: .Europe, population: 613_894, area: 2_586)
+    }
+    
+    public static var Malta: WPCountry {
+        return WPCountry(systemName: "Malta", isoCode: "MT", continent: .Europe, population: 493_559, area: 316)
+    }
+    
+    public static var Moldova: WPCountry {
+        return WPCountry(systemName: "Moldova", isoCode: "MD", continent: .Europe, population: 4_033_963, area: 33_846)
+    }
+    
+    public static var Monaco: WPCountry {
+        return WPCountry(systemName: "Monaco", isoCode: "MC", continent: .Europe, population: 38_300, area: 2)
+    }
+    
+    public static var Montenegro: WPCountry {
+        return WPCountry(systemName: "Montenegro", isoCode: "ME", continent: .Europe, population: 620_029, area: 13_812)
+    }
+    
+    public static var Netherlands: WPCountry {
+        return WPCountry(systemName: "Netherlands", isoCode: "NL", continent: .Europe, population: 17_134_872, area: 41_543)
+    }
+    
+    public static var NorthMacedonia: WPCountry {
+        return WPCountry(systemName: "NorthMacedonia", isoCode: "MK", continent: .Europe, population: 2_083_374, area: 25_713)
+    }
+    
+    public static var Norway: WPCountry {
+        return WPCountry(systemName: "Norway", isoCode: "NO", continent: .Europe, population: 5_421_241, area: 323_802)
+    }
+    
+    public static var Poland: WPCountry {
+        return WPCountry(systemName: "Poland", isoCode: "PL", continent: .Europe, population: 38_433_600, area: 312_696)
+    }
+    
+    public static var Portugal: WPCountry {
+        return WPCountry(systemName: "Portugal", isoCode: "PT", continent: .Europe, population: 10_196_709, area: 92_090)
+    }
+    
+    public static var Romania: WPCountry {
+        return WPCountry(systemName: "Romania", isoCode: "RO", continent: .Europe, population: 19_237_691, area: 238_397)
+    }
+    
+    public static var Russia: WPCountry {
+        return WPCountry(systemName: "Russia", isoCode: "RU", continent: .Europe, population: 146_452_123, area: 17_098_246)
+    }
+    
+    public static var SanMarino: WPCountry {
+        return WPCountry(systemName: "SanMarino", isoCode: "SM", continent: .Europe, population: 33_860, area: 61)
+    }
+    
+    public static var Serbia: WPCountry {
+        return WPCountry(systemName: "Serbia", isoCode: "RS", continent: .Europe, population: 8_737_371, area: 88_361)
+    }
+    
+    public static var Slovakia: WPCountry {
+        return WPCountry(systemName: "Slovakia", isoCode: "SK", continent: .Europe, population: 5_459_642, area: 49_035)
+    }
+    
+    public static var Slovenia: WPCountry {
+        return WPCountry(systemName: "Slovenia", isoCode: "SI", continent: .Europe, population: 2_078_654, area: 20_273)
+    }
+    
+    public static var Spain: WPCountry {
+        return WPCountry(systemName: "Spain", isoCode: "ES", continent: .Europe, population: 46_754_784, area: 505_990)
+    }
+    
+    public static var Sweden: WPCountry {
+        return WPCountry(systemName: "Sweden", isoCode: "SE", continent: .Europe, population: 10_379_295, area: 450_295)
+    }
+    
+    public static var Switzerland: WPCountry {
+        return WPCountry(systemName: "Switzerland", isoCode: "CH", continent: .Europe, population: 8_372_000, area: 41_290)
+    }
+    
+    public static var Ukraine: WPCountry {
+        return WPCountry(systemName: "Ukraine", isoCode: "UA", continent: .Europe, population: 44_246_156, area: 603_628)
+    }
+    
+    public static var UnitedKingdom: WPCountry {
+        return WPCountry(systemName: "UnitedKingdom", isoCode: "GB", continent: .Europe, population: 66_796_807, area: 242_500)
+    }
+    
+    public static var VaticanCity: WPCountry {
+        return WPCountry(systemName: "VaticanCity", isoCode: "VA", continent: .Europe, population: 523, area: 1)
+    }
+    
+}
