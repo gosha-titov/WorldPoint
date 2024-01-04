57f58fa38c6eccb23b194f49e776bd6f63391776
gosha <gosha.titov@icloud.com>
2024-01-04 18:04:26 +0300

    Feat: add Australian countries

---
+++ Sources/Countries/AustralianCountries.swift
@@ -0,0 +1,127 @@
+extension WPCountry {
+    
+    /// Returns all countries located on the Australia continent.
+    public static var allAustralianCountries: [WPCountry] {
+        return [
+            Australia, AmericanSamoa,
+            CookIslands,
+            Fiji, FrenchPolynesia,
+            Guam,
+            Kiribati,
+            MarshallIslands, Micronesia,
+            Nauru, NewCaledonia, NewZealand, Niue, NorfolkIsland, NorthernMarianaIslands,
+            OutlyingIslands,
+            Palau, PapuaNewGuinea, PitcairnIslands,
+            Samoa, SolomonIslands,
+            Tokelau, Tonga, Tuvalu,
+            Vanuatu,
+            WallisAndFutuna
+        ]
+    }
+    
+    
+    public static var Australia: WPCountry {
+        return WPCountry(systemName: "Australia", isoCode: "AU", continent: .Australia, population: 25_499_884, area: 7_692_024)
+    }
+    
+    public static var AmericanSamoa: WPCountry {
+        return WPCountry(systemName: "AmericanSamoa", isoCode: "AS", continent: .Australia, population: 45_035, area: 199)
+    }
+    
+    public static var CookIslands: WPCountry {
+        return WPCountry(systemName: "CookIslands", isoCode: "CK", continent: .Australia, population: 17_459, area: 236)
+    }
+    
+    public static var Fiji: WPCountry {
+        return WPCountry(systemName: "Fiji", isoCode: "FJ", continent: .Australia, population: 896_445, area: 18_274)
+    }
+    
+    public static var FrenchPolynesia: WPCountry {
+        return WPCountry(systemName: "FrenchPolynesia", isoCode: "PF", continent: .Australia, population: 304_032, area: 4_167)
+    }
+    
+    public static var Guam: WPCountry {
+        return WPCountry(systemName: "Guam", isoCode: "GU", continent: .Australia, population: 170_534, area: 549)
+    }
+    
+    public static var Kiribati: WPCountry {
+        return WPCountry(systemName: "Kiribati", isoCode: "KI", continent: .Australia, population: 119_449, area: 811)
+    }
+    
+    public static var MarshallIslands: WPCountry {
+        return WPCountry(systemName: "MarshallIslands", isoCode: "MH", continent: .Australia, population: 59_190, area: 181)
+    }
+    
+    public static var Micronesia: WPCountry {
+        return WPCountry(systemName: "Micronesia", isoCode: "FM", continent: .Australia, population: 548_914, area: 702)
+    }
+    
+    public static var Nauru: WPCountry {
+        return WPCountry(systemName: "Nauru", isoCode: "NR", continent: .Australia, population: 10_824, area: 21)
+    }
+    
+    public static var NewCaledonia: WPCountry {
+        return WPCountry(systemName: "NewCaledonia", isoCode: "NC", continent: .Australia, population: 271_030, area: 18_576)
+    }
+    
+    public static var NewZealand: WPCountry {
+        return WPCountry(systemName: "NewZealand", isoCode: "NZ", continent: .Australia, population: 4_822_233, area: 270_467)
+    }
+    
+    public static var Niue: WPCountry {
+        return WPCountry(systemName: "Niue", isoCode: "NU", continent: .Australia, population: 1_620, area: 262)
+    }
+    
+    public static var NorfolkIsland: WPCountry {
+        return WPCountry(systemName: "NorfolkIsland", isoCode: "NF", continent: .Australia, population: 2_169, area: 35)
+    }
+    
+    public static var NorthernMarianaIslands: WPCountry {
+        return WPCountry(systemName: "NorthernMarianaIslands", isoCode: "MP", continent: .Australia, population: 49_481, area: 464)
+    }
+    
+    public static var OutlyingIslands: WPCountry {
+        return WPCountry(systemName: "OutlyingIslands", isoCode: "UM", continent: .Australia, population: 316, area: 50)
+    }
+    
+    public static var Palau: WPCountry {
+        return WPCountry(systemName: "Palau", isoCode: "PW", continent: .Australia, population: 18_094, area: 459)
+    }
+    
+    public static var PapuaNewGuinea: WPCountry {
+        return WPCountry(systemName: "PapuaNewGuinea", isoCode: "PG", continent: .Australia, population: 18_094, area: 459)
+    }
+    
+    public static var PitcairnIslands: WPCountry {
+        return WPCountry(systemName: "PitcairnIslands", isoCode: "PN", continent: .Australia, population: 67, area: 49)
+    }
+    
+    public static var Samoa: WPCountry {
+        return WPCountry(systemName: "Samoa", isoCode: "WS", continent: .Australia, population: 18_094, area: 459)
+    }
+    
+    public static var SolomonIslands: WPCountry {
+        return WPCountry(systemName: "SolomonIslands", isoCode: "SB", continent: .Australia, population: 686_884, area: 28_896)
+    }
+    
+    public static var Tokelau: WPCountry {
+        return WPCountry(systemName: "Tokelau", isoCode: "TK", continent: .Australia, population: 1_411, area: 11)
+    }
+    
+    public static var Tonga: WPCountry {
+        return WPCountry(systemName: "Tonga", isoCode: "TO", continent: .Australia, population: 105_695, area: 747)
+    }
+    
+    public static var Tuvalu: WPCountry {
+        return WPCountry(systemName: "Tuvalu", isoCode: "TV", continent: .Australia, population: 11_792, area: 26)
+    }
+    
+    public static var Vanuatu: WPCountry {
+        return WPCountry(systemName: "Vanuatu", isoCode: "VU", continent: .Australia, population: 307_145, area: 12_189)
+    }
+    
+    public static var WallisAndFutuna: WPCountry {
+        return WPCountry(systemName: "WallisAndFutuna", isoCode: "WF", continent: .Australia, population: 15_289, area: 274)
+    }
+    
+}
