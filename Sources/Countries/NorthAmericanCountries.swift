18204b2700bec3001d8978545b5092f637a88801
gosha <gosha.titov@icloud.com>
2024-01-04 18:41:37 +0300

    Feat: add North American countries

---
+++ Sources/Countries/NorthAmericanCountries.swift
@@ -0,0 +1,117 @@
+extension WPCountry {
+    
+    /// Returns all countries located on the North America continent.
+    public static var allNorthAmericanCountries: [WPCountry] {
+        return [
+            AntiguaAndBarbuda,
+            Bahamas, Barbados, Belize,
+            Canada, CostaRica, Cuba,
+            Dominica, DominicanRepublic,
+            ElSalvador,
+            Grenada, Guatemala,
+            Haiti, Honduras,
+            Jamaica,
+            Mexico,
+            Nicaragua,
+            Panama,
+            SaintKittsAndNevis, SaintLucia, SaintVincentAndTheGrenadines,
+            TrinidadAndTobago,
+            UnitedStates
+        ]
+    }
+    
+    
+    public static var AntiguaAndBarbuda: WPCountry {
+        return WPCountry(systemName: "AntiguaAndBarbuda", isoCode: "AG", continent: .NorthAmerica, population: 97_120, area: 442)
+    }
+    
+    public static var Bahamas: WPCountry {
+        return WPCountry(systemName: "Bahamas", isoCode: "BS", continent: .NorthAmerica, population: 389_410, area: 13_878)
+    }
+    
+    public static var Barbados: WPCountry {
+        return WPCountry(systemName: "Barbados", isoCode: "BB", continent: .NorthAmerica, population: 287_025, area: 430)
+    }
+    
+    public static var Belize: WPCountry {
+        return WPCountry(systemName: "Belize", isoCode: "BZ", continent: .NorthAmerica, population: 398_050, area: 22_966)
+    }
+    
+    public static var Canada: WPCountry {
+        return WPCountry(systemName: "Canada", isoCode: "CA", continent: .NorthAmerica, population: 37_364_117, area: 9_984_670)
+    }
+    
+    public static var CostaRica: WPCountry {
+        return WPCountry(systemName: "CostaRica", isoCode: "CR", continent: .NorthAmerica, population: 5_048_007, area: 51_100)
+    }
+    
+    public static var Cuba: WPCountry {
+        return WPCountry(systemName: "Cuba", isoCode: "CU", continent: .NorthAmerica, population: 11_333_483, area: 110_860)
+    }
+    
+    public static var Dominica: WPCountry {
+        return WPCountry(systemName: "Dominica", isoCode: "DM", continent: .NorthAmerica, population: 71_808, area: 750)
+    }
+    
+    public static var DominicanRepublic: WPCountry {
+        return WPCountry(systemName: "DominicanRepublic", isoCode: "DO", continent: .NorthAmerica, population: 10_738_958, area: 48_670)
+    }
+    
+    public static var ElSalvador: WPCountry {
+        return WPCountry(systemName: "ElSalvador", isoCode: "SV", continent: .NorthAmerica, population: 6_453_553, area: 21_041)
+    }
+    
+    public static var Grenada: WPCountry {
+        return WPCountry(systemName: "Grenada", isoCode: "GD", continent: .NorthAmerica, population: 112_003, area: 344)
+    }
+    
+    public static var Guatemala: WPCountry {
+        return WPCountry(systemName: "Guatemala", isoCode: "GT", continent: .NorthAmerica, population: 17_581_472, area: 108_889)
+    }
+    
+    public static var Haiti: WPCountry {
+        return WPCountry(systemName: "Haiti", isoCode: "HT", continent: .NorthAmerica, population: 11_263_077, area: 27_750)
+    }
+    
+    public static var Honduras: WPCountry {
+        return WPCountry(systemName: "Honduras", isoCode: "HN", continent: .NorthAmerica, population: 9_746_117, area: 112_492)
+    }
+    
+    public static var Jamaica: WPCountry {
+        return WPCountry(systemName: "Jamaica", isoCode: "JM", continent: .NorthAmerica, population: 2_948_279, area: 10_991)
+    }
+    
+    public static var Mexico: WPCountry {
+        return WPCountry(systemName: "Mexico", isoCode: "MX", continent: .NorthAmerica, population: 126_190_788, area: 1_964_375)
+    }
+    
+    public static var Nicaragua: WPCountry {
+        return WPCountry(systemName: "Nicaragua", isoCode: "NI", continent: .NorthAmerica, population: 6_486_201, area: 130_373)
+    }
+    
+    public static var Panama: WPCountry {
+        return WPCountry(systemName: "Panama", isoCode: "PA", continent: .NorthAmerica, population: 4_218_808, area: 75_417)
+    }
+    
+    public static var SaintKittsAndNevis: WPCountry {
+        return WPCountry(systemName: "SaintKittsAndNevis", isoCode: "KN", continent: .NorthAmerica, population: 52_823, area: 261)
+    }
+    
+    public static var SaintLucia: WPCountry {
+        return WPCountry(systemName: "SaintLucia", isoCode: "LC", continent: .NorthAmerica, population: 182_790, area: 616)
+    }
+    
+    public static var SaintVincentAndTheGrenadines: WPCountry {
+        return WPCountry(systemName: "SaintVincentAndTheGrenadines", isoCode: "VC", continent: .NorthAmerica, population: 110_608, area: 389)
+    }
+    
+    public static var TrinidadAndTobago: WPCountry {
+        return WPCountry(systemName: "TrinidadAndTobago", isoCode: "TT", continent: .NorthAmerica, population: 1_399_488, area: 5_128)
+    }
+    
+    public static var UnitedStates: WPCountry {
+        return WPCountry(systemName: "UnitedStates", isoCode: "US", continent: .NorthAmerica, population: 331_002_651, area: 9_631_418)
+    }
+    
+}
+
