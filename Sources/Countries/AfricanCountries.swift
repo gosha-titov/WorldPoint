128ee0690786c7d88ad4245e1f3cd4453e0bc4c0
gosha <gosha.titov@icloud.com>
2024-01-06 15:04:42 +0300

    Feat: add African countries

---
+++ Sources/Countries/AfricanCountries.swift
@@ -0,0 +1,255 @@
+extension WPCountry {
+    
+    /// Returns all countries located on the Africa continent.
+    public static var allAfricanCountries: [WPCountry] {
+        return [
+            Algeria, Angola, AscensionIsland,
+            Benin, Botswana, BurkinaFaso, Burundi,
+            CaboVerde, Cameroon, CentralAfricanRepublic, CeutaAndMelilla, Chad, Comoros, CongoBrazzaville, CongoKinshasa,
+            Djibouti,
+            Egypt, EquatorialGuinea, Eritrea, Eswatini, Ethiopia,
+            Gabon, Gambia, Ghana, Guinea, GuineaBissau,
+            IvoryCoast,
+            Kenya,
+            Lesotho, Liberia, Libya,
+            Madagascar, Malawi, Mali, Mauritania, Mauritius, Morocco, Mozambique,
+            Namibia, Niger, Nigeria,
+            Rwanda,
+            SaoTomeAndPrincipe, Senegal, Seychelles, SierraLeone, Somalia, SouthAfrica, SouthSudan, Sudan,
+            Tanzania, Togo, Tunisia,
+            Uganda,
+            WesternSahara,
+            Zambia, Zimbabwe
+        ]
+    }
+    
+    
+    public static var Algeria: WPCountry {
+        return WPCountry(systemName: "Algeria", isoCode: "DZ", continent: .Africa, population: 43_851_044, area: 2_381_741)
+    }
+    
+    public static var Angola: WPCountry {
+        return WPCountry(systemName: "Angola", isoCode: "AO", continent: .Africa, population: 32_866_272, area: 1_246_700)
+    }
+    
+    public static var AscensionIsland: WPCountry {
+        return WPCountry(systemName: "AscensionIsland", isoCode: "AC", continent: .Africa, population: 1_023, area: 88)
+    }
+    
+    public static var Benin: WPCountry {
+        return WPCountry(systemName: "Benin", isoCode: "BJ", continent: .Africa, population: 12_123_200, area: 112_622)
+    }
+    
+    public static var Botswana: WPCountry {
+        return WPCountry(systemName: "Botswana", isoCode: "BW", continent: .Africa, population: 2_351_627, area: 581_730)
+    }
+    
+    public static var BurkinaFaso: WPCountry {
+        return WPCountry(systemName: "BurkinaFaso", isoCode: "BF", continent: .Africa, population: 20_903_273, area: 274_200)
+    }
+    
+    public static var Burundi: WPCountry {
+        return WPCountry(systemName: "Burundi", isoCode: "BI", continent: .Africa, population: 11_890_784, area: 27_834)
+    }
+    
+    public static var CaboVerde: WPCountry {
+        return WPCountry(systemName: "CaboVerde", isoCode: "CV", continent: .Africa, population: 555_987, area: 4_033)
+    }
+    
+    public static var Cameroon: WPCountry {
+        return WPCountry(systemName: "Cameroon", isoCode: "CM", continent: .Africa, population: 26_545_863, area: 475_442)
+    }
+    
+    public static var CentralAfricanRepublic: WPCountry {
+        return WPCountry(systemName: "CentralAfricanRepublic", isoCode: "CF", continent: .Africa, population: 4_829_767, area: 622_984)
+    }
+    
+    public static var CeutaAndMelilla: WPCountry {
+        return WPCountry(systemName: "CeutaAndMelilla", isoCode: "EA", continent: .Africa, population: 165_690, area: 32)
+    }
+    
+    public static var Chad: WPCountry {
+        return WPCountry(systemName: "Chad", isoCode: "TD", continent: .Africa, population: 16_425_864, area: 1_284_000)
+    }
+    
+    public static var Comoros: WPCountry {
+        return WPCountry(systemName: "Comoros", isoCode: "KM", continent: .Africa, population: 869_601, area: 1_862)
+    }
+    
+    public static var CongoBrazzaville: WPCountry {
+        return WPCountry(systemName: "CongoBrazzaville", isoCode: "CG", continent: .Africa, population: 5_518_087, area: 342_000)
+    }
+    
+    public static var CongoKinshasa: WPCountry {
+        return WPCountry(systemName: "CongoKinshasa", isoCode: "CD", continent: .Africa, population: 89_561_403, area: 2_344_858)
+    }
+    
+    public static var Djibouti: WPCountry {
+        return WPCountry(systemName: "Djibouti", isoCode: "DJ", continent: .Africa, population: 988_000, area: 23_200)
+    }
+    
+    public static var Egypt: WPCountry {
+        return WPCountry(systemName: "Egypt", isoCode: "EG", continent: .Africa, population: 102_334_404, area: 1_001_450)
+    }
+    
+    public static var EquatorialGuinea: WPCountry {
+        return WPCountry(systemName: "EquatorialGuinea", isoCode: "GQ", continent: .Africa, population: 1_402_985, area: 28_051)
+    }
+    
+    public static var Eritrea: WPCountry {
+        return WPCountry(systemName: "Eritrea", isoCode: "ER", continent: .Africa, population: 3_546_421, area: 117_600)
+    }
+    
+    public static var Eswatini: WPCountry {
+        return WPCountry(systemName: "Eswatini", isoCode: "SZ", continent: .Africa, population: 1_160_164, area: 17_364)
+    }
+    
+    public static var Ethiopia: WPCountry {
+        return WPCountry(systemName: "Ethiopia", isoCode: "ET", continent: .Africa, population: 128_151_149, area: 1_104_300)
+    }
+    
+    public static var Gabon: WPCountry {
+        return WPCountry(systemName: "Gabon", isoCode: "GA", continent: .Africa, population: 2_225_734, area: 267_668)
+    }
+    
+    public static var Gambia: WPCountry {
+        return WPCountry(systemName: "Gambia", isoCode: "GM", continent: .Africa, population: 2_416_668, area: 10_380)
+    }
+    
+    public static var Ghana: WPCountry {
+        return WPCountry(systemName: "Ghana", isoCode: "GH", continent: .Africa, population: 31_072_940, area: 238_533)
+    }
+    
+    public static var Guinea: WPCountry {
+        return WPCountry(systemName: "Guinea", isoCode: "GN", continent: .Africa, population: 13_132_795, area: 245_857)
+    }
+    
+    public static var GuineaBissau: WPCountry {
+        return WPCountry(systemName: "GuineaBissau", isoCode: "GW", continent: .Africa, population: 1_968_001, area: 36_125)
+    }
+    
+    public static var IvoryCoast: WPCountry {
+        return WPCountry(systemName: "IvoryCoast", isoCode: "CI", continent: .Africa, population: 26_378_274, area: 322_463)
+    }
+    
+    public static var Kenya: WPCountry {
+        return WPCountry(systemName: "Kenya", isoCode: "KE", continent: .Africa, population: 53_771_296, area: 580_367)
+    }
+    
+    public static var Lesotho: WPCountry {
+        return WPCountry(systemName: "Lesotho", isoCode: "LS", continent: .Africa, population: 2_142_249, area: 30_355)
+    }
+    
+    public static var Liberia: WPCountry {
+        return WPCountry(systemName: "Liberia", isoCode: "LR", continent: .Africa, population: 5_057_681, area: 111_369)
+    }
+    
+    public static var Libya: WPCountry {
+        return WPCountry(systemName: "Libya", isoCode: "LY", continent: .Africa, population: 6_871_292, area: 1_759_540)
+    }
+    
+    public static var Madagascar: WPCountry {
+        return WPCountry(systemName: "Madagascar", isoCode: "MG", continent: .Africa, population: 27_691_018, area: 587_041)
+    }
+    
+    public static var Malawi: WPCountry {
+        return WPCountry(systemName: "Malawi", isoCode: "MW", continent: .Africa, population: 19_129_952, area: 118_484)
+    }
+    
+    public static var Mali: WPCountry {
+        return WPCountry(systemName: "Mali", isoCode: "ML", continent: .Africa, population: 20_250_833, area: 1_240_192)
+    }
+    
+    public static var Mauritania: WPCountry {
+        return WPCountry(systemName: "Mauritania", isoCode: "MR", continent: .Africa, population: 4_649_658, area: 1_030_700)
+    }
+    
+    public static var Mauritius: WPCountry {
+        return WPCountry(systemName: "Mauritius", isoCode: "MU", continent: .Africa, population: 1_271_768, area: 2_040)
+    }
+    
+    public static var Morocco: WPCountry {
+        return WPCountry(systemName: "Morocco", isoCode: "MA", continent: .Africa, population: 36_910_560, area: 446_550)
+    }
+    
+    public static var Mozambique: WPCountry {
+        return WPCountry(systemName: "Mozambique", isoCode: "MZ", continent: .Africa, population: 31_255_435, area: 801_590)
+    }
+    
+    public static var Namibia: WPCountry {
+        return WPCountry(systemName: "Namibia", isoCode: "NA", continent: .Africa, population: 2_540_905, area: 824_292)
+    }
+    
+    public static var Niger: WPCountry {
+        return WPCountry(systemName: "Niger", isoCode: "NE", continent: .Africa, population: 24_206_644, area: 1_267_000)
+    }
+    
+    public static var Nigeria: WPCountry {
+        return WPCountry(systemName: "Nigeria", isoCode: "NG", continent: .Africa, population: 206_139_589, area: 923_768)
+    }
+    
+    public static var Rwanda: WPCountry {
+        return WPCountry(systemName: "Rwanda", isoCode: "RW", continent: .Africa, population: 12_952_218, area: 26_338)
+    }
+    
+    public static var SaoTomeAndPrincipe: WPCountry {
+        return WPCountry(systemName: "SaoTomeAndPrincipe", isoCode: "ST", continent: .Africa, population: 219_159, area: 964)
+    }
+    
+    public static var Senegal: WPCountry {
+        return WPCountry(systemName: "Senegal", isoCode: "SN", continent: .Africa, population: 16_743_927, area: 196_722)
+    }
+    
+    public static var Seychelles: WPCountry {
+        return WPCountry(systemName: "Seychelles", isoCode: "SC", continent: .Africa, population: 98_347, area: 452)
+    }
+    
+    public static var SierraLeone: WPCountry {
+        return WPCountry(systemName: "SierraLeone", isoCode: "SL", continent: .Africa, population: 7_976_983, area: 71_740)
+    }
+    
+    public static var Somalia: WPCountry {
+        return WPCountry(systemName: "Somalia", isoCode: "SO", continent: .Africa, population: 15_893_222, area: 637_657)
+    }
+    
+    public static var SouthAfrica: WPCountry {
+        return WPCountry(systemName: "SouthAfrica", isoCode: "ZA", continent: .Africa, population: 59_308_690, area: 1_221_037)
+    }
+    
+    public static var SouthSudan: WPCountry {
+        return WPCountry(systemName: "SouthSudan", isoCode: "SS", continent: .Africa, population: 11_193_725, area: 644_329)
+    }
+    
+    public static var Sudan: WPCountry {
+        return WPCountry(systemName: "Sudan", isoCode: "SD", continent: .Africa, population: 43_849_260, area: 1_861_484)
+    }
+    
+    public static var Tanzania: WPCountry {
+        return WPCountry(systemName: "Tanzania", isoCode: "TZ", continent: .Africa, population: 59_734_218, area: 947_303)
+    }
+    
+    public static var Togo: WPCountry {
+        return WPCountry(systemName: "Togo", isoCode: "TG", continent: .Africa, population: 8_278_724, area: 56_785)
+    }
+    
+    public static var Tunisia: WPCountry {
+        return WPCountry(systemName: "Tunisia", isoCode: "TN", continent: .Africa, population: 11_818_619, area: 163_610)
+    }
+    
+    public static var Uganda: WPCountry {
+        return WPCountry(systemName: "Uganda", isoCode: "UG", continent: .Africa, population: 45_741_007, area: 241_038)
+    }
+    
+    public static var WesternSahara: WPCountry {
+        return WPCountry(systemName: "WesternSahara", isoCode: "EH", continent: .Africa, population: 598_385, area: 266_000)
+    }
+    
+    public static var Zambia: WPCountry {
+        return WPCountry(systemName: "Zambia", isoCode: "ZM", continent: .Africa, population: 18_383_955, area: 752_618)
+    }
+    
+    public static var Zimbabwe: WPCountry {
+        return WPCountry(systemName: "Zimbabwe", isoCode: "ZW", continent: .Africa, population: 14_862_924, area: 390_757)
+    }
+    
+}
