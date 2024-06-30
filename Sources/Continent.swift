62ae353c8fb01a8e306fa6f52c1f94fd8372c94a
gosha <gosha.titov@icloud.com>
2024-07-01 02:15:36 +0300

    Feat: add the `allCountries` property

--- Sources/Continent.swift
+++ Sources/Continent.swift
@@ -1,7 +1,7 @@
 /// A continent that is any of several large geographical regions.
 ///
 /// The list of continents: `Asia`, `Africa`, `Europe`, `NorthAmerica`, `SouthAmerica`, `Australia` and `Antartica`.
-public enum WPContinent: CaseIterable {
+public enum WPContinent: String, CaseIterable {
     case Asia
     case Africa
     case Europe
@@ -9,4 +9,22 @@ public enum WPContinent: CaseIterable {
     case SouthAmerica
     case Australia
     case Antartica
+}
+
+
+extension WPContinent {
+    
+    /// An array of all countries located on this continent.
+    public var allCountries: [WPCountry] {
+        return switch self {
+        case .Asia: WPCountry.allAsianCountries
+        case .Africa: WPCountry.allAfricanCountries
+        case .Europe: WPCountry.allEuropeanCountries
+        case .NorthAmerica: WPCountry.allNorthAmericanCountries
+        case .SouthAmerica: WPCountry.allSouthAmericanCountries
+        case .Australia: WPCountry.allAustralianCountries
+        case .Antartica: []
+        }
+    }
+    
 }
